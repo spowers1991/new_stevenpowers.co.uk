12cad176494d9ba84e5e2c30899d9f48a3a8da90
spowers1991 <stevenpowers1991@gmail.com>
2022-05-04 22:43:16 +0100

    netlify form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ContentBox from './components/ContentBox'
-import Form from './components/Form'
+import ContentBox from '../components/ContentBox'
+import Form from '../components/Form'
 
 const Test = () => { 
     return (        
