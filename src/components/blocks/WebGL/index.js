ad49a9df3f109741f44f71db8ea12c8d42b917aa
spowers1991 <stevenpowers1991@gmail.com>
2023-01-15 20:49:47 +0000

    block animations

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom'; 
 import { Unity, useUnityContext } from "react-unity-webgl";
 
 
@@ -9,7 +10,6 @@ const WebGL = () => {
     setFullScreen(true)
     fullscreen !== false &&
     setFullScreen(false)
-    console.log(fullscreen)
   }
 
   const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
@@ -21,8 +21,16 @@ const WebGL = () => {
 
   const loadingPercentage = Math.round(loadingProgression * 100);
 
+    
+  const location = useLocation();
+  const [state, setState] = useState(false)
+
+  useEffect(() => {
+      setState(true)
+  }, [location]);
+
   return (
-  <div className={`relative max-w-5xl px-10 mt-10 overflow-hidden mx-auto `}>
+  <div className={`${fullscreen ? 'fixed w-full h-full z-30 top-0 left-0' : 'relative max-w-5xl mt-10 mx-auto'}  ${ state ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[5px]'} transform ease-in-out transition-all duration-200 delay-300 px-10`}>
       <div className={`${fullscreen && 'hidden'} mb-10 text-sm`}>
         <h3>
           <b>
@@ -31,18 +39,18 @@ const WebGL = () => {
         </h3>
         <p>
           New Target: 0-8 (Planets in order from the Sun. The Sun mapped to 0.) <br/>
-          Panning: w,a,s,d <br/>
-          Pitching: q, e  <br/>
+          Panning & Zooming: W,A,S,D <br/>
+          Pitch & Yaw: Q, E  <br/>
         </p>
       </div>
       <div className={`${fullscreen ? 'fixed w-full h-full z-30 top-0 left-0' : 'relative max-w-5xl mt-10'} overflow-hidden duration-300 transform`}>
-        <div className={`${isLoaded ? 'opacity-0' : 'opacity-100'} fixed w-full h-full  bg-black text-white text-left pointer-events-none duration-[1s] delay-[3s] flex item-center`}>          
-            <span className="m-auto">Loading... ({loadingPercentage}%) </span>       
-        </div>
-        <div className={`${fullscreen ? 'block' : 'hidden'} fixed top-20 right-20 z-50 text-white text-7xl cursor-pointer close-icon`} onClick={() => setFullScreen(false)} >
-          X
-        </div>
-        <Unity className="w-full h-full" unityProvider={unityProvider} />
+          <div className={`${isLoaded ? 'opacity-0' : 'opacity-100'} fixed w-full h-full  bg-black text-white text-left pointer-events-none duration-[1s] delay-[3s] flex item-center`}>          
+              <span className="m-auto">Loading... ({loadingPercentage}%) </span>       
+          </div>
+          <div className={`${fullscreen ? 'block' : 'hidden'} fixed top-20 right-20 z-50 text-white text-7xl cursor-pointer close-icon`} onClick={() => setFullScreen(false)} >
+            X
+          </div>
+          <Unity className="w-full h-full" unityProvider={unityProvider} />
       </div>
       <div className={`rounded text-xs relative inline-block lg:mt-0 text-l text-black py-3 mt-5 text-center group cursor-pointer`}  onClick={() => fullScreenToggle()}>
             Fullscreen
