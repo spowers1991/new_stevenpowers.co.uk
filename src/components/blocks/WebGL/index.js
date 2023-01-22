6e52b6f740cce1e1398c418caa378cc359f06b65
spowers1991 <stevenpowers1991@gmail.com>
2023-01-22 01:13:04 +0000

    refactor(Webgl): use onProgress rather than loadingProgression from the destructured variables from the useUnityContext to change the percentage value sequentially

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom'; 
 import { Unity, useUnityContext } from "react-unity-webgl";
 
-
 const WebGL = () => {  
 
   const [fullscreen, setFullScreen] = useState(false)
@@ -12,16 +11,14 @@ const WebGL = () => {
     setFullScreen(false)
   }
 
-  const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
+  const { unityProvider, isLoaded } = useUnityContext({
     loaderUrl: "/build/solar-system-build.loader.js",
     dataUrl: "/build/solar-system-build.data.br",
     frameworkUrl: "/build/solar-system-build.framework.js.br",
     codeUrl: "/build/solar-system-build.wasm.br",
   });
 
-  const loadingPercentage = Math.round(loadingProgression * 100);
-
-    
+  const [loadingPercentage, setLoadingPercentage] = useState(0); 
   const location = useLocation();
   const [state, setState] = useState(false)
 
@@ -50,7 +47,7 @@ const WebGL = () => {
           <div className={`${fullscreen ? 'block' : 'hidden'} fixed top-20 right-20 z-50 text-white text-7xl cursor-pointer close-icon`} onClick={() => setFullScreen(false)} >
             X
           </div>
-          <Unity className="w-full h-full" unityProvider={unityProvider} />
+          <Unity className="w-full h-full" onProgress={(progress) => setLoadingPercentage(Math.round(progress * 100))} unityProvider={unityProvider} />
       </div>
       <div className={`rounded text-xs relative inline-block lg:mt-0 text-l text-black py-3 mt-5 text-center group cursor-pointer`}  onClick={() => fullScreenToggle()}>
             Fullscreen
