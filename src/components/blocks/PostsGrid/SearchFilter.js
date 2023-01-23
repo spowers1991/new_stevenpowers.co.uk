71e4ef8fd3693aaa01583d4c3299995b26fec63d
spowers1991 <stevenpowers1991@gmail.com>
2023-01-23 20:00:49 +0000

    polish search bar and lightbox with close featured

--- src/components/blocks/PostsGrid/SearchFilter.js
+++ src/components/blocks/PostsGrid/SearchFilter.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useRef} from 'react';
 
 const SearchFilter = ({ searchTerm, setSearchTerm, filteredData, setFilteredData, posts }) => {
+    
+    const inputRef = useRef(null);
+
     return (
         <div className="max-w-5xl mx-auto px-10 mb-5">
-            <input className="rounded block w-full my-8 p-2  border-2 border-black border-solid focus:border-solid placeholder-shown:border-dashed focus:outline-none focus:placeholder:text-black text-sm" placeholder='Type to search...'  type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+            <div className="relative">
+            <input ref={inputRef} className="rounded block w-full my-8 p-2  border-2 border-black border-solid focus:border-solid placeholder-shown:border-dashed focus:outline-none focus:placeholder:text-black text-sm" placeholder='Type to search...'  type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+                { searchTerm !== '' && 
+                    <button className="absolute top-0 right-2 bottom-0 text-2xl z-20" onClick={() => setSearchTerm('')}>
+                        X
+                    </button>
+                }
+            </div>
         </div>
     )
 }
