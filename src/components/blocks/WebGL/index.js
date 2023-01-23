c94df8b25d2014f1132ba5171bf65aadf668b681
spowers1991 <stevenpowers1991@gmail.com>
2023-01-23 23:33:31 +0000

    test

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -12,7 +12,7 @@ const WebGL = () => {
     setFullScreen(false)
   }
 
-  const { unityProvider, isLoaded, loadingProgression ,unityContext} = useUnityContext({
+  const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
     loaderUrl: "/build/solar-system-build.loader.js",
     dataUrl: "/build/solar-system-build.data.br",
     frameworkUrl: "/build/solar-system-build.framework.js.br",
@@ -42,18 +42,9 @@ const WebGL = () => {
     return () => clearInterval(interval);
   }, [loadingProgression, loadingPercentage]);
 
-  useEffect(() => {
-    setState(true);
-
-    return () => {
-        if (unityContext && isLoaded) {
-            unityContext.on("quitted", function () {});
-            console.log('Clean up here!')
-            setState(false);
-        }
-    };
-}, [location, unityContext, isLoaded]);
-
+  if (location.pathname.includes('/pages/webgl/')) {
+    console.log('webgl!')
+  }
 
   return (
   <div className={`${fullscreen ? 'fixed w-full h-full z-30 top-0 left-0' : 'relative max-w-5xl mt-10 mx-auto'}  ${ state ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[5px]'} transform ease-in-out transition-all duration-200 delay-300 px-10`}>
