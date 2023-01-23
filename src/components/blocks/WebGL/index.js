d6dbb55cd2469254421de3e3abb003fa4bd569e9
spowers1991 <stevenpowers1991@gmail.com>
2023-01-23 20:32:13 +0000

    refactor: unmount webgl on location change

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -42,6 +42,16 @@ const WebGL = () => {
     return () => clearInterval(interval);
   }, [loadingProgression, loadingPercentage]);
 
+  useEffect(() => {
+    setState(true);
+    return () => {
+        setState(false);
+        if (isLoaded) {
+            Unity.Unmount();
+        }
+    };
+}, [location, isLoaded]);
+
   return (
   <div className={`${fullscreen ? 'fixed w-full h-full z-30 top-0 left-0' : 'relative max-w-5xl mt-10 mx-auto'}  ${ state ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[5px]'} transform ease-in-out transition-all duration-200 delay-300 px-10`}>
       <div className={`${fullscreen && 'hidden'} mb-10 text-sm`}>
