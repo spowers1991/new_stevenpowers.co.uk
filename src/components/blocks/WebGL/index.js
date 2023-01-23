7ccd6017e3674c42726e3e852e60c3d53082907b
spowers1991 <stevenpowers1991@gmail.com>
2023-01-23 23:05:04 +0000

    new code

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -46,11 +46,14 @@ const WebGL = () => {
     setState(true);
 
     return () => {
-      unityContext.on("quitted", function () {});
-        console.log('Clean up here!')
-        setState(false);
+        if (unityContext && isLoaded) {
+            unityContext.on("quitted", function () {});
+            console.log('Clean up here!')
+            setState(false);
+        }
     };
-}, [location, unityContext]);
+}, [location, unityContext, isLoaded]);
+
 
   return (
   <div className={`${fullscreen ? 'fixed w-full h-full z-30 top-0 left-0' : 'relative max-w-5xl mt-10 mx-auto'}  ${ state ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[5px]'} transform ease-in-out transition-all duration-200 delay-300 px-10`}>
