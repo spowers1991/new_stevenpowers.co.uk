431b4b42968b2a253cbbc2b911dc231f2aec9e31
spowers1991 <stevenpowers1991@gmail.com>
2023-01-23 21:14:43 +0000

    fix 3

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -43,7 +43,6 @@ const WebGL = () => {
   }, [loadingProgression, loadingPercentage]);
 
   return (
-  location.pathname === '/pages/webgl' && 
   <div className={`${fullscreen ? 'fixed w-full h-full z-30 top-0 left-0' : 'relative max-w-5xl mt-10 mx-auto'}  ${ state ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[5px]'} transform ease-in-out transition-all duration-200 delay-300 px-10`}>
       <div className={`${fullscreen && 'hidden'} mb-10 text-sm`}>
         <h3>
