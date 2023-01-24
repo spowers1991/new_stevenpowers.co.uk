e4a1ef24ad0f023ca20d3b8e41f2380eb95efee5
spowers1991 <stevenpowers1991@gmail.com>
2023-01-24 17:32:42 +0000

    test23

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -12,7 +12,7 @@ const WebGL = () => {
     setFullScreen(false)
   }
 
-  const { unityProvider, isLoaded, loadingProgression, unload, unityContext } = useUnityContext({
+  const { unityProvider, isLoaded, loadingProgression, unload } = useUnityContext({
     loaderUrl: "/build/solar-system-build.loader.js",
     dataUrl: "/build/solar-system-build.data.br",
     frameworkUrl: "/build/solar-system-build.framework.js.br",
@@ -45,11 +45,11 @@ const WebGL = () => {
 
   useEffect(() => {
     return () => {
-      unityContext.quitUnityInstance()
       unload();
       window.removeEventListener('keypress')
     }
-  }, [unityContext,unload])
+  }, [unload])
+
 
   return (
   <div className={`${fullscreen ? 'fixed w-full h-full z-30 top-0 left-0' : 'relative max-w-5xl mt-10 mx-auto'}  ${ state ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[5px]'} transform ease-in-out transition-all duration-200 delay-300 px-10`}>
