78500c27b487ee5b77dc1505c6ffecb458a347e6
spowers1991 <stevenpowers1991@gmail.com>
2023-01-24 00:50:08 +0000

    test14

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -49,7 +49,7 @@ const WebGL = () => {
       unload();
       window.removeEventListener('keypress', unload)
     }
-  }, [isLoaded, unload])
+  }, [location, unload])
 
   return (
   <div className={`${fullscreen ? 'fixed w-full h-full z-30 top-0 left-0' : 'relative max-w-5xl mt-10 mx-auto'}  ${ state ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[5px]'} transform ease-in-out transition-all duration-200 delay-300 px-10`}>
