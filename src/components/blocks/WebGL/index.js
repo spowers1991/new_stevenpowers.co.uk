6c261daf9b65553e9f561e739211b7e53b31c5ee
spowers1991 <stevenpowers1991@gmail.com>
2023-01-25 21:59:42 +0000

    abort controller test8

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom'; 
 import { Unity, useUnityContext } from "react-unity-webgl";
-import axios from 'axios';
+
 
 const WebGL = () => {  
 
@@ -11,14 +11,12 @@ const WebGL = () => {
     fullscreen !== false &&
     setFullScreen(false)
   }
-  
-  const [source] = useState(axios.CancelToken.source());
+
   const { unityProvider, isLoaded, loadingProgression, unload, removeEventListener } = useUnityContext({
     loaderUrl: "/build/solar-system-build.loader.js",
     dataUrl: "/build/solar-system-build.data.br",
     frameworkUrl: "/build/solar-system-build.framework.js.br",
     codeUrl: "/build/solar-system-build.wasm.br",
-    cancelToken: source.token
   });
     
   const location = useLocation();
@@ -47,30 +45,12 @@ const WebGL = () => {
 
   useEffect(() => {
     return () => {
+      console.log(state, unityProvider, Unity)
       isLoaded &&
       unload();
       removeEventListener('keypress', unload)
     }
-  }, [isLoaded, unload, removeEventListener])
-
-
-  useEffect(() => {
-    if (!isLoaded) {
-      axios.get('/build/solar-system-build.loader.js', { cancelToken: source.token }).catch(function (thrown) {
-        if (axios.isCancel(thrown)) {
-          console.log('Request canceled', thrown.message);
-        } else {
-          // handle error
-        }
-      });
-    }
-    return () => {
-      source.cancel();
-    }
-  }, [isLoaded, source]);
-
-
-
+  }, [isLoaded, unload, removeEventListener, state, unityProvider])
 
 
   return (
