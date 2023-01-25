5d7328995b29c0032331383c65d361c817214efa
spowers1991 <stevenpowers1991@gmail.com>
2023-01-25 18:46:07 +0000

    test55

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -12,19 +12,19 @@ const WebGL = () => {
     setFullScreen(false)
   }
 
-  const { unityProvider, isLoaded, loadingProgression, unload, removeEventListener } = useUnityContext({
-    loaderUrl: "/build/solar-system-build.loader.js",
-    dataUrl: "/build/solar-system-build.data.br",
-    frameworkUrl: "/build/solar-system-build.framework.js.br",
-    codeUrl: "/build/solar-system-build.wasm.br",
-  });
-    
   const location = useLocation();
   const [state, setState] = useState(false)
 
   useEffect(() => {
       setState(true)
   }, [location]);
+
+  const { unityProvider, isLoaded, loadingProgression, unload, removeEventListener } = useUnityContext({
+    loaderUrl: state ? "/build/solar-system-build.loader.js" : '',
+    dataUrl: state ?  "/build/solar-system-build.data.br" : '',
+    frameworkUrl: state ? "/build/solar-system-build.framework.js.br" : '',
+    codeUrl: state ? "/build/solar-system-build.wasm.br" : '',
+  });
 
   const [loadingPercentage, SetLoadingPercentage] = useState(0)
 
