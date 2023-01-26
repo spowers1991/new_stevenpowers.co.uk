e3b89dc2698bd69bbc52d929142a3b34a0a37127
spowers1991 <stevenpowers1991@gmail.com>
2023-01-26 00:20:09 +0000

    test217

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -67,9 +67,7 @@ const WebGL = (props) => {
           <div className={`${fullscreen ? 'block' : 'hidden'} fixed top-20 right-20 z-50 text-white text-7xl cursor-pointer close-icon`} onClick={() => setFullScreen(false)} >
             X
           </div>
-          {props.unityProvider &&
             <Unity className="w-full h-full" unityProvider={props.unityProvider}/>
-          }
       </div>
       <div className={`rounded text-xs relative inline-block lg:mt-0 text-l text-black py-3 mt-5 text-center group cursor-pointer`}  onClick={() => fullScreenToggle()}>
             Fullscreen
