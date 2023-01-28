9b610a6c44d4dd04bb859d3f9d521044210b7938
spowers1991 <stevenpowers1991@gmail.com>
2023-01-28 19:41:26 +0000

    test2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { createContext} from 'react';
+import React, {useEffect, createContext} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -11,24 +11,30 @@ function App() {
 
   const UnityContext = createContext();
 
-  const { unityProvider, isLoaded, loadingProgression } = useUnityContext({
+  const { unityProvider, isLoaded, loadingProgression, unload } = useUnityContext({
     loaderUrl: "/build/solar-system-build.loader.js",
     dataUrl: "/build/solar-system-build.data.br",
     frameworkUrl: "/build/solar-system-build.framework.js.br",
     codeUrl: "/build/solar-system-build.wasm.br",
   });
 
+  useEffect(() => {
+  return () => {
+    (!window.location.pathname.includes('webgl') && isLoaded ) &&
+        unload();
+  }
+  }, [isLoaded, unload]);
 
   return (
     <div>
       <Router>
         <Header/>
-        <UnityContext.Provider value={{isLoaded, loadingProgression }} >
+        <UnityContext.Provider value={{Unity, unityProvider, isLoaded, loadingProgression }} >
             <main>
               <Routes>
                   <Route path="/" element={<Home />} />              
                   <Route path="pages/home" element={<Home />} />
-                  <Route path="pages/webgl" element={<WebGL UnityContext={UnityContext} canvas={ <Unity className="w-full h-full" unityProvider={unityProvider}/>} /> } />    
+                  <Route path="pages/webgl" element={<WebGL UnityContext={UnityContext} /> } />    
                   <Route path="pages/contact" element={<Contact />}  />      
               </Routes>
             </main>
