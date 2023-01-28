2735625dcf212b9f2fa4c3ce9f738c97e4f755a6
spowers1991 <stevenpowers1991@gmail.com>
2023-01-28 18:58:29 +0000

    reset 3

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useLocation } from 'react-router-dom'; 
-import { Unity, useUnityContext } from "react-unity-webgl";
 
-
-const WebGL = () => {  
+const WebGL = (props) => {  
 
   const [fullscreen, setFullScreen] = useState(false)
   function fullScreenToggle() {
@@ -12,12 +10,7 @@ const WebGL = () => {
     setFullScreen(false)
   }
 
-  const { unityProvider, isLoaded, loadingProgression, unload, removeEventListener } = useUnityContext({
-    loaderUrl: "/build/solar-system-build.loader.js",
-    dataUrl: "/build/solar-system-build.data.br",
-    frameworkUrl: "/build/solar-system-build.framework.js.br",
-    codeUrl: "/build/solar-system-build.wasm.br",
-  });
+  const { isLoaded, loadingProgression, unload, removeEventListener } = useContext(props.UnityContext)
     
   const location = useLocation();
   const [state, setState] = useState(false)
@@ -80,9 +73,7 @@ const WebGL = () => {
           <div className={`${fullscreen ? 'block' : 'hidden'} fixed top-20 right-20 z-50 text-white text-7xl cursor-pointer close-icon`} onClick={() => setFullScreen(false)} >
             X
           </div>
-          <iframe id="iframeDiv" src="" title="twat">
-          <Unity className="w-full h-full" unityProvider={unityProvider}/>
-          </iframe>
+         {props.canvas}
       </div>
       <div className={`rounded text-xs relative inline-block lg:mt-0 text-l text-black py-3 mt-5 text-center group cursor-pointer`}  onClick={() => fullScreenToggle()}>
             Fullscreen
