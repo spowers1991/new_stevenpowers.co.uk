4ba836516645a6eac41376d2ecd5ba68c38ae402
spowers1991 <stevenpowers1991@gmail.com>
2023-01-28 14:22:18 +0000

    try again3

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -67,7 +67,7 @@ const WebGL = (props) => {
           <div className={`${fullscreen ? 'block' : 'hidden'} fixed top-20 right-20 z-50 text-white text-7xl cursor-pointer close-icon`} onClick={() => setFullScreen(false)} >
             X
           </div>
-          <props.canvas className="w-full h-full" />
+          <canvas className="w-full h-full"  />
       </div>
       <div className={`rounded text-xs relative inline-block lg:mt-0 text-l text-black py-3 mt-5 text-center group cursor-pointer`}  onClick={() => fullScreenToggle()}>
             Fullscreen
