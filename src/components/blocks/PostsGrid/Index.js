3a7324210ff5213ffc2742496cef678a927a9e0c
spowers1991 <stevenpowers1991@gmail.com>
2023-01-31 20:15:18 +0000

    style changes

--- src/components/blocks/PostsGrid/Index.js
+++ src/components/blocks/PostsGrid/Index.js
@@ -44,7 +44,7 @@ return (
     <div>  
         <div className={`mt-8 ${ state ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[5px]'} transform ease-in-out transition-all duration-200 delay-300`}>
             <SearchFilter searchTerm={searchTerm} setSearchTerm={setSearchTerm} filteredData={filteredData} setFilteredData={setFilteredData} posts={posts} />
-            <div className="max-w-5xl mx-auto px-10 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-x-8 gap-y-16 md:gap-y-16 mt-10">
+            <div className="max-w-5xl mx-auto px-10 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-4 gap-x-8 gap-y-16 md:gap-y-16 mt-10">
                 {filteredData && filteredData.map((post, index) => (
                     <Post index={index} post={post} key={index} SwiperSyncedKey={SwiperSyncedKey} HandlePopUp={HandlePopUp} open={open}/>
                 ))
