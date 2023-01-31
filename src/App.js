58a2ee9584fd3eb8c9224680bb8c172ce6b5b4b8
spowers1991 <stevenpowers1991@gmail.com>
2023-01-31 02:46:47 +0000

    asddas

--- src/App.js
+++ src/App.js
@@ -23,14 +23,17 @@ function App() {
     (!window.location.pathname.includes('webgl') && isLoaded) &&
     unload()
     return () => {
-      (!window.location.pathname.includes('webgl') && isLoaded) &&
           window.document.removeEventListener('keypress', unload)
           const scripts = document.getElementsByTagName('script')
           const scriptsArray = [...scripts]
           scriptsArray.map((script) => (
-            (script.src.includes("solar-system-build.framework.js") &&  script.src.includes("solar-system-build.wasm")) &&
+            script.src.includes("solar-system-build.framework.js") &&
             script.remove()
         ))
+        scriptsArray.map((script) => (
+          script.src.includes("solar-system-build.wasm.js") &&
+          script.remove()
+      ))
       }
     }, [isLoaded, unload]);
 
