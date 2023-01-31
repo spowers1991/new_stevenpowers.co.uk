8828089735843169bfa98bfd15f25f7c5c3c6f36
spowers1991 <stevenpowers1991@gmail.com>
2023-01-31 15:39:00 +0000

    test

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Heading from './utility/Heading'
 import CaptionReveal from './utility/CaptionReveal'
 import { useLocation } from "react-router-dom";
+import PersistentWebGL from '../components/blocks/WebGL'
 
 const Page = (props) => {
     const { pathname } = useLocation();
@@ -9,6 +10,16 @@ const Page = (props) => {
     useEffect(() => {
       window.scrollTo(0, 0);
     }, [pathname]);
+
+    const [showPersistentWebGL, setShowPersistentWebGL] = useState(false);
+
+  useEffect(() => {
+    if (window.location.pathname.includes('webgl')) {
+      setShowPersistentWebGL(true);
+    } else {
+      setShowPersistentWebGL(false);
+    }
+  }, []);
 
     return (    
         <div className={`overflow-hidden min-h-[550px] `}>
@@ -20,6 +31,7 @@ const Page = (props) => {
             props.blocks &&                                              
                 props.blocks            
             }          
+            {showPersistentWebGL && <PersistentWebGL />}
         </div>           
     )
 }
--- src/pages/webgl.js
+++ src/pages/webgl.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Page from '../components/Page'
-import WebGL from '../components/blocks/WebGL/index';
 
 const WebGl = () => {  
     return (        
         <div>     
-            <Page title="Solar System in WebGL" caption="Here is a WebGL Solar System I created and imported into React..." blocks={<WebGL />}/>
+            <Page title="Solar System in WebGL" caption="Here is a WebGL Solar System I created and imported into React..." />
         </div>           
     )
 }
