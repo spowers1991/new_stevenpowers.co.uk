3baaaffb5e5409b9c6a105f31c296cf30b6b7b26
spowers1991 <stevenpowers1991@gmail.com>
2023-01-31 15:05:25 +0000

    test

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -49,7 +49,7 @@ const WebGL = () => {
         const scripts = document.getElementsByTagName('script')
         const scriptsArray = [...scripts]
         scriptsArray.map((script) => (
-          (script.src.includes("solar-system-build.framework.js") ||  script.src.includes("solar-system-build.wasm")) &&
+          script.src.includes("solar-system-build.framework.js") &&
           script.remove()
         ))     
       }
