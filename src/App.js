31e77640c6a09924af1033351e1bd2972446fe62
spowers1991 <stevenpowers1991@gmail.com>
2023-01-31 02:36:12 +0000

    asddas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, {useEffect, createContext} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/home'
 import Contact from './pages/contact'
 import WebGL from './pages/webgl'
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 function App() {
+
+  const UnityContext = createContext();
+
+  const { unityProvider, isLoaded, loadingProgression, unload } = useUnityContext({
+    loaderUrl: "/build/solar-system-build.loader.js",
+    dataUrl: "/build/solar-system-build.data.br",
+    frameworkUrl: "/build/solar-system-build.framework.js.br",
+    codeUrl: "/build/solar-system-build.wasm.br",
+  });
+
+  
+  useEffect(() => {
+    (!window.location.pathname.includes('webgl') && isLoaded) &&
+    unload()
+    return () => {
+          window.document.removeEventListener('keypress', unload)
+          const scripts = document.getElementsByTagName('script')
+          const scriptsArray = [...scripts]
+          scriptsArray.map((script) => (
+            script.src.includes("solar-system-build") &&
+            script.remove()
+        ))
+      }
+    }, [isLoaded, unload]);
 
   return (
     <div>
       <Router>
         <Header/>
+          <UnityContext.Provider value={{Unity, unityProvider, isLoaded, loadingProgression }} >
             <main>
               <Routes>
                   <Route path="/" element={<Home />} />              
                   <Route path="pages/home" element={<Home />} />
-                  <Route path="pages/webgl" element={<WebGL /> } />    
+                  <Route path="pages/webgl" element={<WebGL UnityContext={UnityContext} canvas={ <Unity className="w-full h-full" unityProvider={unityProvider}/> } /> } />    
                   <Route path="pages/contact" element={<Contact />}  />      
               </Routes>
             </main>
+          </UnityContext.Provider>
         <Footer />    
       </Router>
     </div>
