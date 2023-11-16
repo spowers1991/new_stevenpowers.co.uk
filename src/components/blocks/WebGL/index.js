e9527f8fafbb659d9aa128ec14b87454314aecb5
spowers1991 <stevenpowers1991@gmail.com>
2023-11-16 15:23:11 +0000

    chore: update the webgl solar system to make it look more photorealistic and improve the camera controller

--- src/components/blocks/WebGL/index.js
+++ src/components/blocks/WebGL/index.js
@@ -56,8 +56,7 @@ const WebGL = () => {
       <div className={`${fullscreen ? 'fixed w-full h-full z-30 top-0 left-0' : 'relative max-w-5xl mt-10 mx-auto'}  ${ state ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-[5px]'} transform ease-in-out transition-all duration-200 delay-300 px-10`}>
         <div className={`${fullscreen && 'hidden'} mb-10 text-sm`}>
           <div className="p-3 mb-5 rounded border-black border-2 ">
-            Unfortunely, due to a bug in the Unity WebGL compiler we have to initiate a hard refresh of the application to clear all the scripts after leaving the page that Unity is mounted on. Otherwise the WebGL app will take priority over certain keypress event, making them inaccessable to the rest of the app. Unity are
-            aware of the issue but currently there is no fix.
+            This experience is currently for <b>Desktop only</b>.
           </div>
           <h3>
             <b>
